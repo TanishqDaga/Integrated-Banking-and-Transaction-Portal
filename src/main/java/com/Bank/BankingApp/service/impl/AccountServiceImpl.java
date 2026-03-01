09066ebab6ba10b37432af3d9de95f6ac62c9f3d
TanishqDaga <tanishqdaga7@gmail.com>
2026-03-01 14:31:48 +0530

    deleteAccount REST API added

--- src/main/java/com/Bank/BankingApp/service/impl/AccountServiceImpl.java
+++ src/main/java/com/Bank/BankingApp/service/impl/AccountServiceImpl.java
@@ -71,6 +71,15 @@ public class AccountServiceImpl implements AccountService {
 				.collect(Collectors.toList());
 	}
 
+	@Override
+	public void deleteAccount(Long id) {
+		Account account= accountRepository
+				.findById(id)
+				.orElseThrow(() -> new RuntimeException("Account does not exists"));
+		accountRepository.deleteById(id);
+		
+	}
+
 	
 	
 
